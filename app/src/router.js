6ac836ec16154ae589aac0731704c7ae04257779
Abraham Post <abrahampost@yahoo.com>
2019-02-07 12:36:03 -0500

    Added login page

--- app/src/router.js
+++ app/src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import Login from './views/Login.vue';
 
 Vue.use(Router);
 
@@ -9,9 +9,9 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home,
+      path: '/login',
+      name: 'login',
+      component: Login,
     },
     {
       path: '/about',
@@ -21,5 +21,9 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
     },
+    {
+      path: '/*',
+      redirect: '/login'
+    }
   ],
 });
---
+++ app/src/views/Login.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="login">
+    <div class="ui card padded">
+      <form class="ui form" @submit.prevent="login">
+        <h1 class="ui subheader">Login</h1>
+        <div class="field">
+          <label>Username</label>
+          <input type="text"
+                v-model="username"
+                name="username" 
+                placeholder="Username"
+                autocomplete="username">
+        </div>
+        <div class="field">
+          <label>Password</label>
+          <input type="password" 
+                v-model="password"
+                name="password" 
+                placeholder="Password"
+                autocomplete="current-password">
+        </div>
+        <button class="ui button" type="submit">Submit</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Login',
+  data: function () {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    login: function() {
+      console.log(this.username, btoa(this.password));
+    }
+  }
+};
+</script>
+<style scoped>
+  .login {
+    height: 90vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .padded {
+    padding: 5%;
+  }
+</style>
