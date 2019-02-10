4c8ddd38e57d8533d9238912a2a5d9ef12648e9c
Richard Le <richardle798@gmail.com>
2019-02-09 20:53:21 -0500

    implemented user match and patient x attribute tables

--- db/drop-tables.js
+++ db/drop-tables.js
@@ -1,7 +1,9 @@
 const User              = require("../models/user"),
       PatientInfo       = require("../models/patient-info"),
       PatientXParent    = require("../models/patient-x-parent"),
-      Attribute         = require("../models/attribute");
+      Attribute         = require("../models/attribute"),
+      PatientXAttribute = require("../models/patient-x-attribute"),
+      UserMatch         = require("../models/user_match");
 
 async function dropTables() {
     try {
@@ -13,6 +15,10 @@ async function dropTables() {
         console.log("Successfully dropped patient-x-parent table");
         await Attribute.drop();
         console.log("Successfully dropped attribute table");
+        await PatientXAttribute.drop();
+        console.log("Successfully dropped patient-x-attribute table");
+        await UserMatch.drop();
+        console.log("Successfully dropped user-match table");
         process.exit(0);
     } catch (e) {
         console.error(e);
--- db/init-db.js
+++ db/init-db.js
@@ -1,7 +1,9 @@
 const User              = require("../models/user"),
       PatientInfo       = require("../models/patient-info"),
       PatientXParent    = require("../models/patient-x-parent"),
-      Attribute         = require("../models/attribute");
+      Attribute         = require("../models/attribute"),
+      PatientXAttribute = require("../models/patient-x-attribute"),
+      UserMatch         = require("../models/user_match");
 
 async function createTables() {
     try {
@@ -13,6 +15,10 @@ async function createTables() {
         console.log("Successfully created patient-x-parent table");
         await Attribute.sync();
         console.log("Successfully created attribute table");
+        await PatientXAttribute.sync();
+        console.log("Successfully created patient-x-attribute table");
+        await UserMatch.sync();
+        console.log("Successfully created user-match table");
         process.exit(0);
     } catch (e) {
         console.error(e);
