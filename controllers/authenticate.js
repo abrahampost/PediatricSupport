877ecc92044dbbd999c474698cb646b048060373
Cameron <cameter44@yahoo.com>
2019-02-18 17:33:32 -0500

    Moved the location of helper method.

--- controllers/authenticate.js
+++ controllers/authenticate.js
@@ -19,6 +19,8 @@ router.post('/login', async function(req, res) {
 
 router.post('/signup', async function(req, res) {
     try {
+        let username = generateRandom();
+        let password = generateRandom();
         let email = req.body.email;
         let type = req.body.type;
         let lastName = req.body.lastName;
@@ -29,5 +31,29 @@ router.post('/signup', async function(req, res) {
         res.status(400).send({error: "Could not create user account"});
     }
 });
+
+let generateRandom = exports.generate = () => {
+    let firstWord = ["Busy", "Nimble", "Brave", "Mighty", "Clever", "Proud",
+        "Fair", "Wise", "Loyal", "Happy", "Cheerful", "Joyful", "Friendly", "Powerful",
+        "Excited", "Calm", "Alert", "Tough", "Polite", "Amusing", "Kind", "Gentle", "Caring",
+        "Good", "Cozy", "Great", "Beautiful", "Glowing", "Snug"];
+
+    let lastWord = ["Shepherd", "Cake", "Moon", "Apple", "Banana", "Bike",
+        "Clover", "Crowd", "Lake", "Pear", "River", "Road", "Rose", "Water",
+        "Chicken", "Deer", "Drum", "Goose", "Grape", "Horse", "Kitten", "Owl", "Spoon",
+        "Ladybug", "Pancake", "Pear", "Quilt", "Scarf", "Stream", "Throne", "Badge", "Magic",
+        "Bubble", "Island", "Lamp", "Marble"];
+
+    let randNum = Math.floor(Math.random() * 100);
+    if (randNum == 13 | randNum == 69) {
+        randNum++;
+    }
+    
+    let randString = firstWord[Math.floor(Math.random() * firstWord.length)] +
+    lastWord[Math.floor(Math.random() * lastWord.length)] + randNum;
+    
+    return randString;
+    
+};
 
 module.exports = router;
