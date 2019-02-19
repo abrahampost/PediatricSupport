6da1cab1b7760f5380b7cbfe6f6d5b11c43ebc72
Abraham Post <abrahampost@yahoo.com>
2019-02-19 12:44:11 -0500

    Added database seed

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/sh
 if [ "$NODE_ENV" != "production" ]; then
     node db/drop-tables.js
+    node db/init-db.js
+    node db/seed-db.js
+else 
+    node db/init-db.js
 fi
 
-node db/init-db.js
+cd app && npm install && npm run build
