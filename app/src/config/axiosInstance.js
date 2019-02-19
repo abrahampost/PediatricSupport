e861236dac39e406a7e53590060d3ca2cff61771
Abraham Post <abrahampost@yahoo.com>
2019-02-19 13:56:16 -0500

    Moved postdeploy to release

--- app/src/config/axiosInstance.js
+++ app/src/config/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   //ionic serve sets the node_env to development. Remote has a node_env of dev or prod, so will use'/' instead
-  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:8080/api' : '/',
+  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:8080/api' : '',
   headers: {
     Authorization: {
       toString() {
--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/sh
-if [ "$NODE_ENV" != "production" ]; then
-    node db/drop-tables.js
-fi
-
+node db/drop-tables.js
 node db/init-db.js
+node db/seed-db.js
