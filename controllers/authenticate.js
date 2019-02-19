6439f21ea1f0c88d49cc372581e8ceffc77cab78
Abraham Post <abrahampost@yahoo.com>
2019-02-18 23:33:28 -0500

    Moved generateRandom

--- controllers/authenticate.js
+++ controllers/authenticate.js
@@ -15,8 +15,8 @@ router.post('/login', async function (req, res, next) {
 
 router.post('/signup', async function(req, res) {
     try {
-        let username = generateRandom();
-        let password = generateRandom();
+        let username = userService.generateRandom();
+        let password = userService.generateRandom();
         let email = req.body.email;
         let type = req.body.type;
         let lastName = req.body.lastName;
@@ -27,29 +27,5 @@ router.post('/signup', async function(req, res) {
         next(e);
     }
 });
-
-let generateRandom = exports.generate = () => {
-    let firstWord = ["Busy", "Nimble", "Brave", "Mighty", "Clever", "Proud",
-        "Fair", "Wise", "Loyal", "Happy", "Cheerful", "Joyful", "Friendly", "Powerful",
-        "Excited", "Calm", "Alert", "Tough", "Polite", "Amusing", "Kind", "Gentle", "Caring",
-        "Good", "Cozy", "Great", "Beautiful", "Glowing", "Snug"];
-
-    let lastWord = ["Shepherd", "Cake", "Moon", "Apple", "Banana", "Bike",
-        "Clover", "Crowd", "Lake", "Pear", "River", "Road", "Rose", "Water",
-        "Chicken", "Deer", "Drum", "Goose", "Grape", "Horse", "Kitten", "Owl", "Spoon",
-        "Ladybug", "Pancake", "Pear", "Quilt", "Scarf", "Stream", "Throne", "Badge", "Magic",
-        "Bubble", "Island", "Lamp", "Marble"];
-
-    let randNum = Math.floor(Math.random() * 100);
-    if (randNum == 13 | randNum == 69) {
-        randNum++;
-    }
-    
-    let randString = firstWord[Math.floor(Math.random() * firstWord.length)] +
-    lastWord[Math.floor(Math.random() * lastWord.length)] + randNum;
-    
-    return randString;
-    
-};
 
 module.exports = router;
--- services/user-service.js
+++ services/user-service.js
@@ -67,6 +67,30 @@ exports.signUp = async function (username, unhashed_password, last_name, first_n
     }
 }
 
+exports.generateRandom = function() {
+    let firstWord = ["Busy", "Nimble", "Brave", "Mighty", "Clever", "Proud",
+        "Fair", "Wise", "Loyal", "Happy", "Cheerful", "Joyful", "Friendly", "Powerful",
+        "Excited", "Calm", "Alert", "Tough", "Polite", "Amusing", "Kind", "Gentle", "Caring",
+        "Good", "Cozy", "Great", "Beautiful", "Glowing", "Snug"];
+
+    let lastWord = ["Shepherd", "Cake", "Moon", "Apple", "Banana", "Bike",
+        "Clover", "Crowd", "Lake", "Pear", "River", "Road", "Rose", "Water",
+        "Chicken", "Deer", "Drum", "Goose", "Grape", "Horse", "Kitten", "Owl", "Spoon",
+        "Ladybug", "Pancake", "Pear", "Quilt", "Scarf", "Stream", "Throne", "Badge", "Magic",
+        "Bubble", "Island", "Lamp", "Marble"];
+
+    let randNum = Math.floor(Math.random() * 100);
+    if (randNum == 13 | randNum == 69) {
+        randNum++;
+    }
+    
+    let randString = firstWord[Math.floor(Math.random() * firstWord.length)] +
+    lastWord[Math.floor(Math.random() * lastWord.length)] + randNum;
+    
+    return randString;
+    
+};
+
 let ValidatePassword = (password) => {
     //TODO: Add more password validations
     if (!password || password.length < 7 || password.length > 40) {
