1a54259e0af79363fae3fee4b3dc97fcbbd7dc54
Abraham Post <abrahampost@yahoo.com>
2019-02-19 13:40:15 -0500

    Updated bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/sh
 if [ "$NODE_ENV" != "production" ]; then
     node db/drop-tables.js
-    node db/init-db.js
-    node db/seed-db.js
-    cd app && npm install && npm run build
 fi
 
 node db/init-db.js
-
