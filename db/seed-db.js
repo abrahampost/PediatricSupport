7e64b3689f5b42079b9d3e51fb7e70cbcbf8e453
Richard Le <richardle798@gmail.com>
2019-02-20 11:44:10 -0500

    changed seed data to more intuitive usernames and passwords (#24)
    * changed seed data to more intuitive usernames and passwords
    
    * changed passwords to be longer then 7 characters

--- db/seed-db.js
+++ db/seed-db.js
@@ -3,10 +3,10 @@ const User = require("./sequelize").user;
 
 async function seedDB() {
     try {
-        await userService.signUp('richardle', 'richardle', 'Le', 'Richard', 'richyle@live.unc.edu', 'patient');
-        await userService.signUp('abrahampost', 'abrahampost', 'Post', 'Abraham', 'abrahamp@live.unc.edu', 'admin');
-        await userService.signUp('cameronterrell', 'cameronterrell', 'Terrell', 'Cameron', 'cameter@live.unc.edu', 'parent');
-        await userService.signUp('alexaguero', 'alexaguero', 'Aguero', 'Alex', 'daguero@live.unc.edu', 'patient');
+        await userService.signUp('patient1', 'patient123', 'Le', 'Richard', 'richyle@live.unc.edu', 'patient');
+        await userService.signUp('admin', 'admin123', 'Post', 'Abraham', 'abrahamp@live.unc.edu', 'admin');
+        await userService.signUp('parent', 'parent123', 'Terrell', 'Cameron', 'cameter@live.unc.edu', 'parent');
+        await userService.signUp('patient2', 'patient123', 'Aguero', 'Alex', 'daguero@live.unc.edu', 'patient');
         console.log("Successfully seeded db");
         process.exit(0);
     } catch(e) {
