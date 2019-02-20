01148208130e6b5dce8afde350ff07a86050b742
Richard Le <richardle798@gmail.com>
2019-02-20 11:41:07 -0500

    changed passwords to be longer then 7 characters

--- db/seed-db.js
+++ db/seed-db.js
@@ -3,10 +3,10 @@ const User = require("./sequelize").user;
 
 async function seedDB() {
     try {
-        await userService.signUp('patient1', 'patient1', 'Le', 'Richard', 'richyle@live.unc.edu', 'patient');
-        await userService.signUp('admin', 'admin', 'Post', 'Abraham', 'abrahamp@live.unc.edu', 'admin');
-        await userService.signUp('parent', 'parent', 'Terrell', 'Cameron', 'cameter@live.unc.edu', 'parent');
-        await userService.signUp('patient2', 'patient2', 'Aguero', 'Alex', 'daguero@live.unc.edu', 'patient');
+        await userService.signUp('patient1', 'patient123', 'Le', 'Richard', 'richyle@live.unc.edu', 'patient');
+        await userService.signUp('admin', 'admin123', 'Post', 'Abraham', 'abrahamp@live.unc.edu', 'admin');
+        await userService.signUp('parent', 'parent123', 'Terrell', 'Cameron', 'cameter@live.unc.edu', 'parent');
+        await userService.signUp('patient2', 'patient123', 'Aguero', 'Alex', 'daguero@live.unc.edu', 'patient');
         console.log("Successfully seeded db");
         process.exit(0);
     } catch(e) {
