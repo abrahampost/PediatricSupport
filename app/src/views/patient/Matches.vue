685e1606cb742094c48da91a0bea24fa2273dfee
Abraham Post <abrahampost@yahoo.com>
2019-02-23 21:44:22 -0500

    Redesign tabs and add sent state

--- app/src/views/patient/Matches.vue
+++ app/src/views/patient/Matches.vue
@@ -1,33 +1,45 @@
 <template>
   <div class="patient-matches">
+    <h1 class="ui huge header margin">Patient Matches</h1>
     <div class="ui container">
-      <h1>Patient Matches</h1>
-      <div class="ui menu">
-        <a class="item" @click="filterType='all'" :class="{'active': 'all' === filterType}">All</a>
-        <a class="item" @click="filterType='matched'" :class="{'active': 'matched' === filterType}">Matched</a>
-        <a class="item" @click="filterType='pending'" :class="{'active': 'pending' === filterType}">Pending</a>
+      <div class="ui top attached tabular four item menu">
+        <a class="item" @click="filterType='all'"
+          :class="{'active': 'all' === filterType}">All</a>
+        <a class="item" @click="filterType='matched'"
+          :class="{'active': 'matched' === filterType}">Matched</a>
+        <a class="item" @click="filterType='pending'"
+          :class="{'active': 'pending' === filterType}">Pending</a>
+        <a class="item" @click="filterType='sent'"
+          :class="{'active': 'sent' === filterType}">Sent</a>
       </div>
-        <div class="ui segment">
-          <div class="ui cards">
-            <div class="ui centered card" v-for="match in filteredMatches" :key="match.id">
-              <div class="image">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png">
+      <div class="ui bottom attached segment">
+        <div class="ui cards">
+          <div class="ui centered card" v-for="match in filteredMatches" :key="match.id">
+            <div class="image">
+              <img src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png">
+            </div>
+            <div class="content">
+              <div class="header">{{ match.user.name }}</div>
+              <div class="description">{{ match.user.description }}</div>
+            </div>
+            <div class="extra content">
+              <div v-if="match.type === 'matched'">
+                <p>Matched</p>
               </div>
-              <div class="content">
-                <div class="header">{{ match.user.name }}</div>
-                <div class="description">{{ match.user.description }}</div>
-              </div>
-              <div class="extra content">
-                <p v-if="match.type != 'pending'">Matched</p>
-                <p v-if="match.type == 'pending'">Respond to Request</p>
+              <div v-if="match.type === 'sent'"
+                class="ui basic red button"
+                @click="deleteMatch(match.id)">Delete Request</div>
+              <div v-if="match.type === 'pending'">
+                <p>Respond to Request</p>
                 <div class="ui two buttons" v-if="match.type == 'pending'">
-                  <div class="ui basic green button" @click="acceptFriend(match.id)">Accept</div>
-                  <div class="ui basic red button" @click="denyFriend(match.id)">Deny</div>
+                  <div class="ui basic green button" @click="acceptMatch(match.id)">Accept</div>
+                  <div class="ui basic red button" @click="denyMatch(match.id)">Deny</div>
                 </div>
               </div>
             </div>
           </div>
         </div>
+      </div>
     </div>
   </div>
 </template>
@@ -76,7 +88,7 @@ export default {
             name: 'LittleWhale25',
             description: 'If you like fortnite pls friend me!!!.',
           },
-          type: 'pending',
+          type: 'sent',
         },
         {
           id: 6,
@@ -106,16 +118,23 @@ export default {
     },
   },
   methods: {
-    acceptFriend(id) {
-      //TODO: Make this hit backend
-      console.log("ACCEPT FRIEND: " + id);
+    acceptMatch(id) {
+      // TODO: Make this hit backend
+      console.log(`ACCEPT FRIEND: ${id}`);
     },
-    denyFriend(id) {
-      //TODO: Make this hit backend
-      console.log("DENY FRIEND: " + id);
-    }
-  }
+    denyMatch(id) {
+      // TODO: Make this hit backend
+      console.log(`DENY FRIEND: ${id}`);
+    },
+    deleteMatch(id) {
+      // TODO: Make this hit the backend
+      console.log(`DELETE MATCH: ${id}`);
+    },
+  },
 };
 </script>
 <style scoped>
+.ui.huge.header.margin {
+  margin: 1em;
+}
 </style>
