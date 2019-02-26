5f190228471f94f4b8e54e8ae185b66d62256073
Richard Le <richardle798@gmail.com>
2019-02-26 13:36:13 -0500

    changed user test cases to use async instead of promises

--- test/user.js
+++ test/user.js
@@ -31,7 +31,7 @@ describe('Users', () => {
             await userService.signUp(testAdmin.username, testAdmin.password, testAdmin.lastName, testAdmin.firstName, testAdmin.email, testAdmin.type);
         });
 
-        it('it should create an account', (done) => {
+        it('it should create an account', async () => {
             let requestBody = {
                 patientFirstName: "John",
                 patientLastName: "Patient",
@@ -40,26 +40,20 @@ describe('Users', () => {
                 parentLastName: "Parent",
                 parentEmail: "geschwat@masafiagrofood.com"
             }
-            chai.request(server)
+            let res = await chai.request(server)
                 .post("/api/authenticate/login")
                 .send({
                     username: testAdmin.username,
                     password: testAdmin.password
-                })
-                .end((err, res) => {
-                    let token = res.body.token;
-                    chai.request(server)
-                        .post("/api/users")
-                        .send(requestBody)
-                        .set('Authorization', token)
-                        .end((err, res) => {
-                            should.not.exist(err);
-                            res.should.have.status(201);
-                            done();
-                        });
-                })
+                });
+            let token = res.body.token;
+            res = await chai.request(server)
+                .post("/api/users")
+                .send(requestBody)
+                .set('Authorization', token);
+            res.should.have.status(201);
         });
-        it('it should not allow creation of an account with missing required data', (done) => {
+        it('it should not allow creation of an account with missing required data', async () => {
             let requestBody = {
                 patientFirstName: "John",
                 patientLastName: "Patient",
@@ -67,26 +61,20 @@ describe('Users', () => {
                 parentLastName: "Parent",
                 parentEmail: "geschwat@masafiagrofood.com"
             }
-            chai.request(server)
+            let res = await chai.request(server)
                 .post("/api/authenticate/login")
                 .send({
                     username: testAdmin.username,
                     password: testAdmin.password
-                })
-                .end((err, res) => {
-                    let token = res.body.token;
-                    chai.request(server)
-                        .post("/api/users")
-                        .send(requestBody)
-                        .set('Authorization', token)
-                        .end((err, res) => {
-                            should.not.exist(err);
-                            res.should.have.status(400);
-                            done();
-                        });
                 });
+            let token = res.body.token;
+            res = await chai.request(server)
+                .post("/api/users")
+                .send(requestBody)
+                .set('Authorization', token);
+            res.should.have.status(400);
         });
-        it('it should not allow creation of an account with an invalid email', (done) => {
+        it('it should not allow creation of an account with an invalid email', async () => {
             let requestBody = {
                 patientFirstName: "John",
                 patientLastName: "Patient",
@@ -95,29 +83,24 @@ describe('Users', () => {
                 parentLastName: "Parent",
                 parentEmail: "geschwat@masafiagrofood.com"
             }
-            chai.request(server)
+            let res = await chai.request(server)
                 .post("/api/authenticate/login")
                 .send({
                     username: testAdmin.username,
                     password: testAdmin.password
-                })
-                .end((err, res) => {
-                    let token = res.body.token;
-                    chai.request(server)
-                        .post("/api/users")
-                        .send(requestBody)
-                        .set('Authorization', token)
-                        .end((err, res) => {
-                            should.not.exist(err);
-                            res.should.have.status(400);
-                            done();
-                        });
                 });
+
+            let token = res.body.token;
+            res = await chai.request(server)
+                .post("/api/users")
+                .send(requestBody)
+                .set('Authorization', token);
+            res.should.have.status(400);
         });
     });
 
     describe("/POST login", () => {
-        it("it should login patient account with proper credentials", (done) => {
+        it("it should login patient account with proper credentials", async () => {
             let user = {
                 username: "johndoe",
                 password: "password",
@@ -126,31 +109,28 @@ describe('Users', () => {
                 email: "johndoe@gmail.com",
                 type: "patient"
             }
-            userService.signUp(user.username, user.password, user.lastName, user.firstName, user.email, user.type).then(() => {
-                chai.request(server)
-                    .post("/api/authenticate/login")
-                    .send({
-                        username: "johndoe",
-                        password: "password"
-                    })
-                    .end((err, res) => {
-                        should.not.exist(err);
-                        res.should.have.status(200);
-                        res.body.should.be.a("object");
-                        res.body.should.have.property("token");
-                        res.body.should.have.property("user");
-                        res.body.user.should.be.a("object");
-                        res.body.user.should.have.property("username");
-                        res.body.user.should.have.property("email");
-                        res.body.user.should.have.property("type");
-                        res.body.user.username.should.be.eql(user.username);
-                        res.body.user.email.should.be.eql(user.email);
-                        res.body.user.type.should.be.eql(user.type, "should be " + user.type);
-                        done();
-                    });
-            });
-        })
-        it("it should login admin account with proper credentials", (done) => {
+            await userService.signUp(user.username, user.password, user.lastName, user.firstName, user.email, user.type);
+            let res = await chai.request(server)
+                .post("/api/authenticate/login")
+                .send({
+                    username: "johndoe",
+                    password: "password"
+                });
+
+            res.should.have.status(200);
+            res.body.should.be.a("object");
+            res.body.should.have.property("token");
+            res.body.should.have.property("user");
+            res.body.user.should.be.a("object");
+            res.body.user.should.have.property("username");
+            res.body.user.should.have.property("email");
+            res.body.user.should.have.property("type");
+            res.body.user.username.should.be.eql(user.username);
+            res.body.user.email.should.be.eql(user.email);
+            res.body.user.type.should.be.eql(user.type, "should be " + user.type);
+        });
+
+        it("it should login admin account with proper credentials", async () => {
             let user = {
                 username: "johndoe",
                 password: "password",
@@ -159,31 +139,27 @@ describe('Users', () => {
                 email: "johndoe@gmail.com",
                 type: "admin"
             }
-            userService.signUp(user.username, user.password, user.lastName, user.firstName, user.email, user.type).then(() => {
-                chai.request(server)
-                    .post("/api/authenticate/login")
-                    .send({
-                        username: "johndoe",
-                        password: "password"
-                    })
-                    .end((err, res) => {
-                        should.not.exist(err);
-                        res.should.have.status(200);
-                        res.body.should.be.a("object");
-                        res.body.should.have.property("token");
-                        res.body.should.have.property("user");
-                        res.body.user.should.be.a("object");
-                        res.body.user.should.have.property("username");
-                        res.body.user.should.have.property("email");
-                        res.body.user.should.have.property("type");
-                        res.body.user.username.should.be.eql(user.username);
-                        res.body.user.email.should.be.eql(user.email);
-                        res.body.user.type.should.be.eql(user.type, "should be " + user.type);
-                        done();
-                    });
-            });
+            await userService.signUp(user.username, user.password, user.lastName, user.firstName, user.email, user.type);
+            let res = await chai.request(server)
+                .post("/api/authenticate/login")
+                .send({
+                    username: "johndoe",
+                    password: "password"
+                });
+            res.should.have.status(200);
+            res.body.should.be.a("object");
+            res.body.should.have.property("token");
+            res.body.should.have.property("user");
+            res.body.user.should.be.a("object");
+            res.body.user.should.have.property("username");
+            res.body.user.should.have.property("email");
+            res.body.user.should.have.property("type");
+            res.body.user.username.should.be.eql(user.username);
+            res.body.user.email.should.be.eql(user.email);
+            res.body.user.type.should.be.eql(user.type, "should be " + user.type);
         });
-        it("it should login parent account with proper credentials", (done) => {
+
+        it("it should login parent account with proper credentials", async () => {
             let user = {
                 username: "johndoe",
                 password: "password",
@@ -192,31 +168,27 @@ describe('Users', () => {
                 email: "johndoe@gmail.com",
                 type: "parent"
             }
-            userService.signUp(user.username, user.password, user.lastName, user.firstName, user.email, user.type).then(() => {
-                chai.request(server)
-                    .post("/api/authenticate/login")
-                    .send({
-                        username: "johndoe",
-                        password: "password"
-                    })
-                    .end((err, res) => {
-                        should.not.exist(err);
-                        res.should.have.status(200);
-                        res.body.should.be.a("object");
-                        res.body.should.have.property("token");
-                        res.body.should.have.property("user");
-                        res.body.user.should.be.a("object");
-                        res.body.user.should.have.property("username");
-                        res.body.user.should.have.property("email");
-                        res.body.user.should.have.property("type");
-                        res.body.user.username.should.be.eql(user.username);
-                        res.body.user.email.should.be.eql(user.email);
-                        res.body.user.type.should.be.eql(user.type, "should be " + user.type);
-                        done();
-                    });
-            });
+            await userService.signUp(user.username, user.password, user.lastName, user.firstName, user.email, user.type);
+            let res = await chai.request(server)
+                .post("/api/authenticate/login")
+                .send({
+                    username: "johndoe",
+                    password: "password"
+                });
+            res.should.have.status(200);
+            res.body.should.be.a("object");
+            res.body.should.have.property("token");
+            res.body.should.have.property("user");
+            res.body.user.should.be.a("object");
+            res.body.user.should.have.property("username");
+            res.body.user.should.have.property("email");
+            res.body.user.should.have.property("type");
+            res.body.user.username.should.be.eql(user.username);
+            res.body.user.email.should.be.eql(user.email);
+            res.body.user.type.should.be.eql(user.type, "should be " + user.type);
         });
-        it("it should not login with bad credentials", (done) => {
+
+        it("it should not login with bad credentials", async () => {
             let user = {
                 username: "johndoe",
                 password: "password",
@@ -225,40 +197,34 @@ describe('Users', () => {
                 email: "johndoe@gmail.com",
                 type: "patient"
             }
-            userService.signUp(user.username, user.password, user.lastName, user.firstName, user.email, user.type).then(() => {
-                chai.request(server)
-                    .post("/api/authenticate/login")
-                    .send({
-                        username: "johndoe",
-                        password: "badpassword"
-                    })
-                    .end((err, res) => {
-                        should.not.exist(err);
-                        res.should.have.status(401);
-                        res.body.should.be.an("object");
-                        res.body.should.have.property('error');
-                        res.body.error.should.be.a('string');
-                        res.body.error.should.be.eql('Incorrect username and password combination.');
-                        done();
-                    });
-            });
+            await userService.signUp(user.username, user.password, user.lastName, user.firstName, user.email, user.type);
+            let res = await chai.request(server)
+                .post("/api/authenticate/login")
+                .send({
+                    username: "johndoe",
+                    password: "badpassword"
+                });
+
+            res.should.have.status(401);
+            res.body.should.be.an("object");
+            res.body.should.have.property('error');
+            res.body.error.should.be.a('string');
+            res.body.error.should.be.eql('Incorrect username and password combination.');
         });
-        it("it should not authenticate when attempting to login a non-existent user", (done) => {
-            chai.request(server)
+
+        it("it should not authenticate when attempting to login a non-existent user", async () => {
+            let res = await chai.request(server)
                 .post("/api/authenticate/login")
                 .send({
                     username: "johndoe5",
                     password: "password"
-                })
-                .end((err, res) => {
-                    should.not.exist(err);
-                    res.should.have.status(401);
-                    res.body.should.be.an("object");
-                    res.body.should.have.property('error');
-                    res.body.error.should.be.a('string');
-                    res.body.error.should.be.eql('Incorrect username and password combination.');
-                    done();
                 });
-        })
+            res.should.have.status(401);
+            res.body.should.be.an("object");
+            res.body.should.have.property('error');
+            res.body.error.should.be.a('string');
+            res.body.error.should.be.eql('Incorrect username and password combination.');
+
+        });
     });
 });
