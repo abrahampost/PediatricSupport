90e62cef9f3c567f587869d3ff1a5d761944216c
Abraham Post <abrahampost@yahoo.com>
2019-02-26 11:40:06 -0500

    Added match routes

--- controllers/index.js
+++ controllers/index.js
@@ -2,5 +2,6 @@ let express = require("express"),
     router  = express.Router();
 
 router.use("/authenticate", require("./authenticate"));
+router.use("/matches", require("./match"));
 
 module.exports = router;
