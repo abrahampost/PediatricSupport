ec3f0c61912c4849975efd68e8efa761a6a05434
Abraham <abrahampost@yahoo.com>
2019-04-03 18:33:48 -0400

    Filtered dropdown with dependency added

--- app/src/views/patient/Preferences.vue
+++ app/src/views/patient/Preferences.vue
@@ -10,7 +10,7 @@
       - Patent's currently saved interests are reflected in checkboxes when page is loaded
       - On submit, updates "current interests" message with selected interests
     -->
-    <div class="ui stackable grid form">
+    <div class="ui stackable grid">
       <div class="four wide column">
         <div class="ui centered card">
           <div class="image">
@@ -22,39 +22,32 @@
         </div>
       </div>
       <div class="twelve wide column">
-        <div class="ui field">
-          <h4 class="ui left aligned header">Biography</h4>
-          <textarea type="text" rows="3" placeholder="Write here..." v-model="biography"></textarea>
+        <div class="ui form">
+          <div class="ui field">
+            <h4 class="ui left aligned header">Biography</h4>
+            <textarea type="text" rows="3" placeholder="Write here..." v-model="biography"></textarea>
+          </div>
         </div>
         <div>
           <h4 class="ui left aligned header">Interests</h4>
-          <div class>
-            <input
-              type="text"
-              list="available-interest-list"
-              placeholder="Add Interest"
-              v-model="selectedInterest"
-              @change="updateSelectedInterest()"
-            >
-            <datalist id="available-interest-list">
-              <option v-for="interest in unselectedInterests" :key="interest.id">{{interest.name}}</option>
-            </datalist>
-          </div>
-          <div class="ui floated segment">
-            <div
-              class="ui floated compact segment"
-              v-for="interest in interests"
+          <v-select
+            label="name"
+            :options="filteredInterests"
+            placeholder="Add Interest"
+            v-model="selectedInterest"
+            @change="updateInterests"
+          ></v-select>
+          <div v-if="selectedInterests.length != 0" class="ui fluid segment">
+            <button
+              class="ui compact circular red button"
+              type="button"
+              v-for="interest in selectedInterests"
               :key="interest.id"
+              @click="deleteInterest(interest.id)"
             >
-              <div v-if="interest.like" class="ui toggle checkbox">
-                <input type="checkbox" @click="handleId(interest.id)" checked="true">
-                <label>{{ interest.name }}</label>
-              </div>
-              <div v-else class="ui toggle checkbox">
-                <input type="checkbox" @click="handleId(interest.id)">
-                <label>{{ interest.name }}</label>
-              </div>
-            </div>
+              <i class="window close outline icon"></i>
+              {{ interest.name }}
+            </button>
           </div>
         </div>
       </div>
@@ -68,76 +61,46 @@ export default {
   data() {
     return {
       selectedInterest: "",
+      selectedInterests: [],
       biography: "",
       interests: [
-        { id: 1, like: true, name: "legos" },
-        { id: 2, like: true, name: "movies" },
-        { id: 3, like: false, name: "sports" },
-        { id: 4, like: true, name: "chess" },
-        { id: 5, like: false, name: "dinosaurs" },
-        { id: 6, like: false, name: "youtube" },
-        { id: 7, like: true, name: "skating" },
-        { id: 8, like: true, name: "videogames" },
-        { id: 9, like: false, name: "broccoli" },
-        { id: 10, like: true, name: "badminton" },
-        { id: 11, like: true, name: "having a really great time" },
-        { id: 12, like: false, name: "cup stacking" },
-        { id: 13, like: false, name: "going to disney land" },
-        { id: 14, like: false, name: "riding a bus around town" },
-        { id: 15, like: true, name: "long walks on the beach" },
-        { id: 16, like: false, name: "water without any ice" },
-        { id: 17, like: false, name: "volunteering at a soup kitchen" },
-        { id: 18, like: false, name: "baking gluten free cookies" }
+        { id: 1, name: "legos" },
+        { id: 2, name: "movies" },
+        { id: 3, name: "sports" },
+        { id: 4, name: "chess" },
+        { id: 5, name: "dinosaurs" },
+        { id: 6, name: "youtube" },
+        { id: 7, name: "skating" },
+        { id: 8, name: "videogames" },
+        { id: 9, name: "broccoli" },
+        { id: 10, name: "badminton" },
+        { id: 11, name: "having a really great time" },
+        { id: 12, name: "cup stacking" },
+        { id: 13, name: "going to disney land" },
+        { id: 14, name: "riding a bus around town" },
+        { id: 15, name: "long walks on the beach" },
+        { id: 16, name: "water without any ice" },
+        { id: 17, name: "volunteering at a soup kitchen" },
+        { id: 18, name: "baking gluten free cookies" }
       ]
     };
   },
-  computed: {
-    upperCaseSelectedInterest: function() {
-      return this.selectedInterest.toUpperCase();
-    },
-    isValidInterest: function() {
-      let filteredInterests = this.filteredUnselectedInterests;
-      return (
-        filteredInterests.length === 1 &&
-        filteredInterests[0].name.toUpperCase() ===
-          this.upperCaseSelectedInterest
+  methods: {
+    deleteInterest(id) {
+      this.selectedInterests = this.selectedInterests.filter(
+        item => item.id != id
       );
     },
-    selectedInterests: function() {
-      return this.interests.filter(interest => {
-        return interest.like;
-      });
-    },
-    unselectedInterests: function() {
-      return this.interests.filter(interest => {
-        return !interest.like;
-      });
-    },
-    filteredUnselectedInterests: function() {
-      if (this.selectedInterest === null) {
-        return [];
-      }
-      return this.interests.filter(interest => {
-        return (
-          !interest.like &&
-          interest.name.toUpperCase().indexOf(this.upperCaseSelectedInterest) >
-            -1
-        );
-      });
-    }
-  },
-  methods: {
-    clickSelectedInterest(interest) {
-      console.log("click");
-      this.selectedInterest = interest.name;
-      this.updateSelectedInterest();
-    },
-    updateSelectedInterest() {
-      console.log("update");
-      if (this.isValidInterest) {
-        this.filteredUnselectedInterests[0].like = true;
+    updateInterests() {
+      if (this.selectedInterest) {
+        this.selectedInterests.push(this.selectedInterest);
         this.selectedInterest = "";
       }
+    }
+  },
+  computed: {
+    filteredInterests() {
+      return this.interests.filter((interest) => this.selectedInterests.indexOf(interest) == -1);
     }
   }
 };
