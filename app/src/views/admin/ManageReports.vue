7773018c38ecbf80459b88d5fe0f43ff7e01e74e
Richard Le <richardle798@gmail.com>
2019-04-18 12:40:26 -0400

    began admin view reports

--- app/src/views/admin/ManageReports.vue
+++ app/src/views/admin/ManageReports.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="admin">
     <h1>Manage Reports</h1>
+    <div class="ui negative message" v-if="error">
+      <div class="header">{{ error }}</div>
+    </div>
   </div>
 </template>
 <script>
 export default {
   name: 'AdminManageReports',
+  data() {
+    return {
+      reports: [],
+      error: ''
+    };
+  },
 };
 </script>
 <style scoped>
